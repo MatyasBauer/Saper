c71ba27bc28788bfab195213e3c5b826f0e376d1
Matyas Bauer <devmtsbauer@gmail.com>
2022-07-25 14:31:26 +0200

    Field generator

--- src/helpers/Field.test.ts
+++ src/helpers/Field.test.ts
@@ -1,6 +1,8 @@
-import { emptyFieldGenerator, CellState } from "./Field";
+import { emptyFieldGenerator, fieldGenerator, CellState, Cell } from "./Field";
 
 const { empty, bomb, hidden } = CellState;
+
+const cellWithBombFilter = (cell: Cell) => cell === bomb;
 
 describe("Field Generator", () => {
   describe("Empty field generator", () => {
@@ -16,6 +18,47 @@ describe("Field Generator", () => {
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
       ]);
+    });
+  });
+  describe("Field generator", () => {
+    it("Wrong propability value", () => {
+      const err = "Propability value must be between 0 and 1";
+      expect(() => fieldGenerator(1, -1)).toThrow(err);
+      expect(() => fieldGenerator(1, 2)).toThrow(err);
+    });
+    it("Smallest possible field without bomb", () => {
+      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+    });
+    it("Smallest possible field with bomb", () => {
+      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
+    });
+    it("2x2 field with bombs", () => {
+      expect(fieldGenerator(2, 1)).toStrictEqual([
+        [bomb, bomb],
+        [bomb, bomb],
+      ]);
+    });
+    it("2x2 field with 50% propability", () => {
+      const field = fieldGenerator(2, 0.5);
+      const flatField = field.flat();
+
+      const cellsWithBombs = flatField.filter((cell) => cell === bomb);
+      const cellsWithoutBombs = flatField.filter((cell) => cell === 2);
+
+      expect(cellsWithBombs).toHaveLength(2);
+      expect(cellsWithoutBombs).toHaveLength(2);
+    });
+    it("10x10 with 1/4 bomb cells", () => {
+      const size = 10;
+      const bombs = 25;
+
+      const propability = bombs / (size * size);
+      const field = fieldGenerator(size, propability);
+
+      console.table(field);
+
+      const flatfield = field.flat();
+      expect(flatfield.filter(cellWithBombFilter)).toHaveLength(bombs);
     });
   });
 });
--- src/helpers/Field.ts
+++ src/helpers/Field.ts
@@ -1,3 +1,5 @@
+import { incrementNeibours } from "./CellsManipulator";
+
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Field = Cell[][];
 export type Coords = [number, number];
@@ -14,3 +16,29 @@ export const emptyFieldGenerator = (
   size: number,
   state: Cell = CellState.empty
 ): Field => new Array(size).fill(null).map(() => new Array(size).fill(state));
+
+export const fieldGenerator = (size: number, propability: number): Field => {
+  if (propability < 0 || propability > 1) {
+    throw new Error("Propability value must be between 0 and 1");
+  }
+
+  let unprocessedCells = size * size;
+  let restCellsWithBomb = unprocessedCells * propability;
+  const result: Field = emptyFieldGenerator(size);
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if (restCellsWithBomb === 0) {
+        return result;
+      }
+      if (restCellsWithBomb / unprocessedCells > Math.random()) {
+        result[y][x] = CellState.bomb;
+        incrementNeibours([y, x], result);
+        restCellsWithBomb--;
+      }
+      unprocessedCells--;
+    }
+  }
+
+  return result;
+};
